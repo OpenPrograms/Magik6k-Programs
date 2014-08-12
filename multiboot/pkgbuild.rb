9b4b33df80bfe3ccb07f7e986ccde0e55bde9d75
Łukasz Magiera <magik6k@gmail.com>
2014-08-12 15:29:18 +0200

    Provide description for multiboot

--- multiboot/pkgbuild.rb
+++ multiboot/pkgbuild.rb
@@ -1,5 +1,7 @@
 id 'multiboot'
 name 'Simple and light bootloader'
+description 'A simple program that allows booting multiple systems. INSTALLING IT WILL OVERWRITE init.lua, install it using -f option. Installing it in OpenOS should be safe'
+
 
 install 'init.lua' => '//init.lua'
 install 'openos.lua' => '//boot/kernel/OpenOS'
